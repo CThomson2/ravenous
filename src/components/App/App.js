c174eab345958507bf693da1d1ab6ffcd73f2efb
CThomson2 <107051481+CThomson2@users.noreply.github.com>
2022-06-14 10:05:13 +0100

    Test (#1)
    * add dog obj
    
    * chg business files
    
    * Part 2 Complete
    
    Changed inter-component functionality to allow App to pass information down to the lower components. Next, the searchbar will be worked on.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,16 +4,37 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Bordertown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [];
+for (let i = 0; i < 6; i++) {
+  businesses.push(business);
+}
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
 }
 
 export default App;
+
+const dog = {
+  name: 'Husky'
+};
